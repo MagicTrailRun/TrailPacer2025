28c9dc28ad11f570ea85d731c989848bf76e20ac
NICOLAS Maelle Valerie <210304@chu.run>
2025-11-06 15:14:19 +0400

    Revert "test st.secrets"
    This reverts commit 97181bae50863a79fd965c089f8b5abf708f5b2a.

--- core/supabase_client.py
+++ core/supabase_client.py
@@ -1,10 +1,19 @@
-from supabase import create_client, Client
+from supabase import create_client
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 import streamlit as st
+from supabase import create_client, Client
 
 def get_supabase_client() -> Client:
     """Retourne une instance isolée du client Supabase pour cet utilisateur."""
     if "supabase_client" not in st.session_state:
-        url = st.secrets["SUPABASE_URL"]
-        key = st.secrets["SUPABASE_KEY"]
+        url = st.secrets["supabase"]["url"]
+        key = st.secrets["supabase"]["anon_key"]
         st.session_state["supabase_client"] = create_client(url, key)
     return st.session_state["supabase_client"]
